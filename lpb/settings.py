7eef1801b62ca17e48cc0d7f078846f045bd0b71
Corwin Cole <coriwn.cole@gmail.com>
2018-01-28 08:26:41 -0700

    Added EB full URL to ALLOWED_HOSTS

--- lpb/settings.py
+++ lpb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'elasticbeanstalk-us-west-1-847240153276.s3-us-west-1.amazonaws.com']
 
 
 # Application definition
