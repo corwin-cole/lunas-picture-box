97e99e5fbb4204474a854e62e47de5253b9ae636
Corwin Cole <coriwn.cole@gmail.com>
2018-01-28 09:02:02 -0700

    Added 172.31.164.134 to ALLOWED_HOSTS due to logs suggesting such on the EC2 instance

--- lpb/settings.py
+++ lpb/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'elasticbeanstalk-us-west-1-847240153276.s3-us-west-1.amazonaws.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    'elasticbeanstalk-us-west-1-847240153276.s3-us-west-1.amazonaws.com',
+    '172.31.164.134',
+]
 
 
 # Application definition
