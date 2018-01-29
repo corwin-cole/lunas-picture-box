056a65dfebcbfb78e7dd9d8151d7c42254f1dd89
Corwin Cole <coriwn.cole@gmail.com>
2018-01-28 19:48:09 -0700

    Created Photo and Category admins

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Photo, Category
 
-# Register your models here.
+
+admin.site.register(Category, admin.ModelAdmin)
+admin.site.register(Photo, admin.ModelAdmin)
